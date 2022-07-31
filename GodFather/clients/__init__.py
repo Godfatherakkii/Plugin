f0a92851b92e9562b1a7f4f99996d9e7f29e02a8
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-07-31 07:58:08 -0700

    Update __init__.py

--- GodFather/clients/__init__.py
+++ GodFather/clients/__init__.py
@@ -1,3 +1,3 @@
 from .client_list import clients_list, client_id
 from .decs import hell_cmd, hell_handler
-from .session import godfather, H2, H3, H4, H5, GodFather
+from .session import godfather, H2, H3, H4, H5, GodFatherBot
