91a26579afbf5c72f49814089100e43c62486a1c
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-07-31 07:16:00 -0700

    Add files via upload

---
+++ GodFather/random_strings/devs.py
@@ -0,0 +1,17 @@
+DEVLIST = ["936481432"]
+
+
+HARMFUL = [
+    "SESSION",
+    "HEROKU_API_KEY",
+    "base64",
+    "bash",
+    "get_me()",
+    "phone",
+    "os.system",
+    "sys.stdout",
+    "sys.stderr",
+    "subprocess",
+    "GODFATHERBOT_SESSION",
+    "session.save()",
+]
