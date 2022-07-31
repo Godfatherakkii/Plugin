f390841e2f1ee4430811deb991d792a06a7a7339
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-07-31 06:25:36 -0700

    Update session.py

--- PluginGod/clients/session.py
+++ PluginGod/clients/session.py
@@ -4,7 +4,7 @@ from telethon import TelegramClient
 from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
 from telethon.sessions import StringSession
 
-from hellbot.config import Config
+from PluginGod.config import Config
 
 
 if Config.GODFATHERBOT_SESSION:
