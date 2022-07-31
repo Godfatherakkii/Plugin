9e8e6d536ec6f6a3cdfabcd654906e0959cdcf91
【༒©ʟɛɢɛռɖHคckeℝ༒】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-07-31 07:12:25 -0700

    Add files via upload

---
+++ GodFather/__main__.py
@@ -0,0 +1,107 @@
+import glob
+import os
+import sys
+
+from pathlib import Path
+from telethon import Button, TelegramClient
+from telethon.utils import get_peer_id
+
+from GodFather import LOGS, bot, tbot
+from GodFather.clients.session import godfather, H2, H3, H4, H5
+from GodFather.config import Config
+from GodFather.utils import join_it, load_module, logger_check, start_msg, update_sudo, plug_channel
+from GodFather.version import __hell__ as hellver
+
+hl = Config.HANDLER
+
+HELL_PIC = "https://telegra.ph/file/311e014e444cc43ac226a.jpg"
+
+
+# Client Starter
+async def hells(session=None, client=None, session_name="Main"):
+    if session:
+        LOGS.info(f"••• Starting Client [{session_name}] •••")
+        try:
+            await client.start()
+            return 1
+        except:
+            LOGS.error(f"Error in {session_name}!! Check & try again!")
+            return 0
+    else:
+        return 0
+
+
+# Load plugins based on config UNLOAD
+async def plug_load(path):
+    files = glob.glob(path)
+    for name in files:
+        with open(name) as hell:
+            path1 = Path(hell.name)
+            shortname = path1.stem
+            if shortname.replace(".py", "") in Config.UNLOAD:
+                os.remove(Path(f"GodFather/plugins/{shortname}.py"))
+            else:
+                load_module(shortname.replace(".py", ""))      
+
+
+# Final checks after startup
+async def hell_is_on(total):
+    await update_sudo()
+    await logger_check(bot)
+    await start_msg(tbot, HELL_PIC, hellver, total)
+    await join_it(bot)
+    await join_it(H2)
+    await join_it(H3)
+    await join_it(H4)
+    await join_it(H5)
+
+
+# Hellbot starter...
+async def start_GodFather():
+    try:
+        tbot_id = await tbot.get_me()
+        Config.BOT_USERNAME = f"@{tbot_id.username}"
+        bot.tgbot = tbot
+        LOGS.info("════❰𝕲𝖔𝖉𝕱𝖙𝖆𝖍𝖊𝖗❱═❍⊱❁")
+        C1 = await hells(Config.GODFATHERBOT_SESSION, bot, "GODFATHERBOT_SESSION")
+        C2 = await hells(Config.SESSION_2, H2, "SESSION_2")
+        C3 = await hells(Config.SESSION_3, H3, "SESSION_3")
+        C4 = await hells(Config.SESSION_4, H4, "SESSION_4")
+        C5 = await hells(Config.SESSION_5, H5, "SESSION_5")
+        await tbot.start()
+        total = C1 + C2 + C3 + C4 + C5
+        LOGS.info("╔════❰𝕲𝖔𝖉𝕱𝖙𝖆𝖍𝖊𝖗❱═❍⊱❁۪۪")
+        LOGS.info("║┣⪼𝕊𝕥𝕒𝕣𝕥𝕚𝕟𝕘. 𝔾𝕠𝕕𝔽𝕥𝕒𝕙𝕖𝕣 𝕌𝕤𝕖𝕣𝔹𝕠𝕥║")
+        LOGS.info("║┣⪼𝕊𝕥𝕒𝕣𝕥𝕚𝕟𝕘. 𝕃𝕠𝕕𝕚𝕟𝕘..............................")
+        LOGS.info("╚══════════════════❍⊱")
+        await plug_load("GodFather/plugins/*.py")
+        await plug_channel(bot, Config.PLUGIN_CHANNEL)
+        LOGS.info(f"""『🔱𝕲𝖔𝖉𝕱𝖙𝖆𝖍𝖊𝖗 𝖀𝖘𝖊𝖗𝕭𝖔𝖙🔱』➙𖤍࿐ IS ON!!! 𝕲𝖔𝖉𝕱𝖙𝖆𝖍𝖊𝖗 VERSION :- 𝕍:𝕒 𝟙.𝟘
+                      TYPE :- " .gpromote @GODFATHERAKKII " OR .help OR .ping CHECK IF I'M ON!
+                      ╔════❰𝕲𝖔𝖉𝕱𝖙𝖆𝖍𝖊𝖗❱═❍⊱❁
+                      ║┣⪼ OWNER - 𝔾𝕠𝕕𝔽𝕒𝕥𝕙𝕖𝕣 𝔸𝕜𝕜𝕚𝕚
+                      ║┣⪼ Ultra Real Stick Bot 
+                      ║┣⪼ CREATOR -@godfatherakkii
+                      ║┣⪼ TELETHON - 1.2.0
+                      ║┣⪼ ✨ 『🔱𝕲 𝖔 𝖉 𝖋 𝖆 𝖙 𝖍 𝖊 𝖗🔱』𝐔𝐬𝐞𝐫𝐛𝐨𝐭✨
+                      ║╰━━━━━━━━━━━━━━━➣
+                      ╚══════════════════❍⊱""")
+        LOGS.info(f"» Total Clients = {str(total)} «")
+        await hell_is_on(total)
+    except Exception as e:
+        LOGS.error(f"{str(e)}")
+        sys.exit()
+
+
+bot.loop.run_until_complete(start_GodFather())
+
+if len(sys.argv) not in (1, 3, 4):
+    bot.disconnect()
+else:
+    try:
+        bot.run_until_disconnected()
+    except ConnectionError:
+        pass
+
+
+# hellbot
